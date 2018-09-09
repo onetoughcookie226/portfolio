7d819d8b0c942e0de182d3b7c1c4878639f10d82
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-09 21:25:02 +0200

    improve styling

---
+++ scripts/question.js
@@ -0,0 +1,18 @@
+var submitQuestion = document.getElementById("question"); 
+    submitQuestion.onclick =  function () {
+        console.log("hey");
+    }
+
+
+
+
+// let question = 'where are you from?';
+//     // substring = 'from'
+
+//     // // console.log(question.includes(substring));
+
+
+// if (question == 'where are you from') {
+//     console.log('great!')
+
+// }
