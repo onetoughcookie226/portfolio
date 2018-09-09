b795bbc29e4f08e834711946e9eee9619e2082f9
onetoughcookie226 <onetoughcookie226@gmail.com>
2018-09-09 11:29:23 +0200

    Adding pages

---
+++ scripts/main.js
@@ -0,0 +1,46 @@
+
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/b.jpg') {
+      myImage.setAttribute ('src','images/w.jpg');
+    } else {
+      myImage.setAttribute ('src','images/b.jpg');
+    }
+}
+
+// Store the value of button and h3 in a variable
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h3');
+
+// Store user input in myName variable using prompt()function
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+//store the data in the browser using localStorage so to retrieve it later
+    localStorage.setItem('name', myName);
+// set the textContent of the heading to a string, plus the user's newly stored name.
+    myHeading.textContent = 'Hey, ' + myName;
+  }
+// check whether the name data exists  
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+//retrieve the stored name using getItem() and set the textContent of the heading to a string, plus the user's name   
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hey, ' + storedName;
+  }
+//Add an onclick event handler
+  myButton.onclick = function() {
+    setUserName();
+  }
+
+// let quseion = 'where are you from?';
+//     substring = 'from'
+
+//     // console.log(question.includes(substring));
+
+// if (question == '') {
+
+// }
